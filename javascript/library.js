7df67dbad44727633ff7de5fc1522fbe1b7fe0be
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2023-02-17 22:47:41 +0300

    put a line between class members

--- javascript/library.js
+++ javascript/library.js
@@ -2,6 +2,7 @@
 class Library {
     // the object books is a private property of the Library class 
     #books;
+    
     // the constructor initiates the private property books
     constructor(books={}) {
         this.#books = books;
