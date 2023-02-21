f07124de9c6ae72438dd3e5d7b85f1128cae030c
SaffetGokcenSen <saffetgokcensen@hotmail.com>
2023-02-21 22:17:42 +0300

    added a function to remove a book and print the whole library

--- javascript/library.js
+++ javascript/library.js
@@ -243,6 +243,131 @@ function deliverBookInfo(evt) {
     parentDiv.appendChild(childDiv2);
 }
 
+// This function removes a book and prints the remaining books when the remove 
+// book button is clicked
+function removePrintBooks() {
+    const bookNameArray = theLibrary.getBookNamesArray();
+    bookNameArray.forEach(bookName => {
+        const nextBook = theLibrary.getBook(bookName);
+
+        // the div containing the book info part and the book buttons part
+        const parentDiv = document.createElement("div");
+        // set the class of the parent div to the class "parent-div"
+        parentDiv.classList.add("parent-div");
+
+        // the part containing the book info
+        const childDiv1 = document.createElement("div");
+        // the class of the childDiv1 is set
+        childDiv1.classList.add("book-info");
+        
+        // p element for displaying the book title
+        const pTitle = document.createElement("p");
+
+        // the title of the book
+        const titleText = document.createTextNode(
+            `Title: ${nextBook.getTitle()}`
+        );
+
+        // the title of the book is written into the paragraph
+        pTitle.appendChild(titleText);
+        // the paragraph with the title of the book is added to the book info 
+        // div
+        childDiv1.appendChild(pTitle);
+
+        // p element for displaying the book author
+        const pAuthor = document.createElement("p");
+        // the author of the book
+        const authorText = document.createTextNode(
+            `Author: ${nextBook.getAuthor()}`
+        );
+        // the author is written into the paragraph
+        pAuthor.appendChild(authorText);
+        // the paragraph with the author of the book is added to the book info 
+        // div
+        childDiv1.appendChild(pAuthor);
+
+        // p element for displaying the book publisher
+        const pPublisher = document.createElement("p");
+        // the publisher of the book
+        const publisherText = document.createTextNode(
+            `Publisher: ${nextBook.getPublisher()}`
+        );
+        // the publisher is written into the paragraph
+        pPublisher.appendChild(publisherText);
+        // the paragraph with the publisher of the book is added to the book 
+        // info div
+        childDiv1.appendChild(pPublisher);
+    
+        // p element for displaying the book publication date
+        const pPublicationDate = document.createElement("p");
+        // the publication date of the book
+        const pubDateText = document.createTextNode(
+            `Publication date: ${nextBook.getPublicationDate()}`
+        );
+        // the publication date is written into the paragraph
+        pPublicationDate.appendChild(pubDateText);
+        // the paragraph with the publication date of the book is added to the 
+        // book info div
+        childDiv1.appendChild(pPublicationDate);
+
+        // p element for displaying the book page number
+        const pPageNumber = document.createElement("p");
+        // the page number of the book
+        const pageNumberText = document.createTextNode(
+            `Number of pages: ${nextBook.getNumOfPages()}`
+        );
+        // the page number is written into the paragraph
+        pPageNumber.appendChild(pageNumberText);
+        // the paragraph with the page number of the book is added to the book 
+        // info div
+        childDiv1.appendChild(pPageNumber);
+
+        // add the book info div to the parent div
+        parentDiv.appendChild(childDiv1);
+        // add the parent div with the book info div and book button div to the 
+        // list
+        divBookList.appendChild(parentDiv);
+    
+        // the part containing the book buttons part
+        const childDiv2 = document.createElement("div");
+        // the class of the book buttons div is set
+        childDiv2.classList.add("book-buttons");
+
+        // create the p element for the remove book button
+        const pRemoveBookButton = document.createElement("p");
+        // create a button to remove the book from the library
+        const removeButton = document.createElement("input");
+        // the type of the button is set
+        removeButton.type = "button";
+        // set the label of the button
+        removeButton.value = "Remove from the library";
+        // the class of the button is set
+        removeButton.classList.add ("remove-book");
+        // add the remove book button to the paragraph
+        pRemoveBookButton.appendChild(removeButton);
+        // the paragraph with the button is added to the button div
+        childDiv2.appendChild(pRemoveBookButton);
+
+        // create the p element for the read-status update book button
+        const pReadStatusUpdateButton = document.createElement("p");
+        // create a button to update the read status of the book
+        const readStatusUpdateButton = document.createElement("input");
+        // the type of the button is set
+        readStatusUpdateButton.type = "button";
+        // set the label of the button
+        readStatusUpdateButton.value = "Update the read status";
+        // the class of the button is set
+        readStatusUpdateButton.classList.add ("update-read-status");
+        // add the read-status update button to the paragraph
+        pReadStatusUpdateButton.appendChild(readStatusUpdateButton);
+        // the paragraph with the button is added to the button div
+        childDiv2.appendChild(pReadStatusUpdateButton);
+
+        // the button div is added to the parent div
+        parentDiv.appendChild(childDiv2);
+    })
+}
+
 // The access to the add-book-button
 const addBookButton = document.getElementById('add-book-button');
 // When the add-book-button is clicked, a form for entering book info appears
